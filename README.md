1e52707b8adaac851c46568dd9dd8ed446601048
Brayden Smith <brayden.smithzimmerman@gmail.com>
2023-10-01 11:12:22 -0500

    added generatepassword function and readme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Description
 
+This is a front-end application to generate random strings of text for use as a password. You can define the length of the password as well as whether you want it to include numbers, special characters, and both lower and uppercase letters.
+
 ## Installation
 
 This repo can be downloaded at https://github.com/broden123/passwordgenerator
@@ -11,3 +13,5 @@ This repo can be downloaded at https://github.com/broden123/passwordgenerator
 Source code for this repo was taken from https://github.com/coding-boot-camp/friendly-parakeet
 
 ## Preview
+
+![Website Preview](/assets/passwordgeneratorpreview.png)
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Assignment code here
-
+let upperCas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCas = "abcdefghijklmnopqrstuvwxyz";
+let numChar = "1234567890";
+let speChar = ' !"#$%&' + "()*+,-./:;<=>?@[]^_`{|}~" + "\\";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +13,55 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generate password
+function generatePassword() {
+  let password = "";
+  let passwordString = "";
+  passwordLength = prompt("Enter Number of Characters (8 - 128)");
+  if (!passwordLength) {
+    alert("Please choose a number in the accepted range!");
+    return generatePassword();
+  }
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose a number in the accepted range!");
+    return generatePassword();
+  }
+  if (passwordLength > 7 && passwordLength < 129) {
+    let UpperSelect = confirm("Include upper case characters? (ok for yes)");
+    if (UpperSelect == true) {
+      passwordString += upperCas;
+    }
+
+    let lowerSelect = confirm("Include lower case characters? (ok for yes)");
+    if (lowerSelect == true) {
+      passwordString += lowerCas;
+    }
+
+    let numSelect = confirm("Include numbers? (ok for yes)");
+    if (numSelect == true) {
+      passwordString += numChar;
+    }
+
+    let specialSelect = confirm("Include special characters? (ok for yes)");
+    if (specialSelect == true) {
+      passwordString += speChar;
+    }
+
+    if (passwordString == "") {
+      alert("Please choose at least one character type to continue");
+      return generatePassword();
+    }
+  }
+  //choose characters from pool at random and return into string at wanted length
+  for (i = 0; i < passwordLength; i++) {
+    password +=
+      passwordString[Math.floor(Math.random() * passwordString.length)];
+  }
+
+  return password;
+}
